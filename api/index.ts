6315538f4fb0598e0a5e3a18e16ee2031969d939
Grant Sherman <tnarg122@gmail.com>
2025-03-08 07:32:22 +0000

    try switching js back to ts for sanity

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { HTTPException } from 'hono/http-exception';
 import { serve } from '@hono/node-server';
-import { authorization } from './middleware/authorization.js';
+import { authorization } from './middleware/authorization.ts';
 import inventory from './routes/inventory.js';
 import shop from './routes/shop.js';
 
