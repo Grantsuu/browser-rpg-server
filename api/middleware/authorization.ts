ba445ac686f06de29d05a85bdadfa5b890f61a30
Grant Sherman <tnarg122@gmail.com>
2025-03-08 07:26:08 +0000

    supabase js

--- api/middleware/authorization.ts
+++ api/middleware/authorization.ts
@@ -1,6 +1,6 @@
 import { createMiddleware } from 'hono/factory';
 import { HTTPException } from 'hono/http-exception';
-import { supabase } from '../lib/supabase.ts';
+import { supabase } from '../lib/supabase.js';
 
 export const authorization = createMiddleware(async (c, next) => {
     const jwt = c.req.header('Authorization')?.replace('Bearer ', '');
