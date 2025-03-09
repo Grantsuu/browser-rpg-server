27e4fc1a29672e91899447678595b2b18583f2d4
Grant Sherman <tnarg122@gmail.com>
2025-03-09 05:28:59 +0000

    starting to migrate crafting to api

--- api/controllers/inventory.ts
+++ api/controllers/inventory.ts
@@ -32,3 +32,74 @@ export const getInventoryByUserId = async (userId: string) => {
         throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
     }
 }
+
+export const findItemInInventory = async (characterId: string, itemId: number) => {
+    try {
+        const { data, error } = await supabase
+            .from('inventories')
+            .select()
+            .eq('character', characterId)
+            .eq('item', itemId);
+
+        if (error) {
+            console.log(error);
+            throw new HTTPException(500, { message: 'unable to search inventory' })
+        }
+
+        return data;
+    } catch (error) {
+        throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
+    }
+}
+
+export const addItemToInventory = async (userId: string, itemId: number, amount: number) => {
+    try {
+        const characterId = await getCharacterIdByUserId(userId);
+        const item = await findItemInInventory(characterId, itemId);
+        if (item) {
+            const { error } = await supabase
+                .from('inventories')
+                .update({ amount: amount + item[0].amount })
+                .eq('character', characterId)
+                .eq('item', itemId)
+            if (error) {
+                console.log(error);
+                throw new HTTPException(500, { message: 'unable to update item in inventory' })
+            }
+        } else {
+            const { error } = await supabase
+                .from('inventories')
+                .insert({
+                    character: characterId,
+                    item: itemId,
+                    amount: amount
+                })
+            if (error) {
+                console.log(error);
+                throw new HTTPException(500, { message: 'unable to add item to inventory' })
+            }
+        }
+        return true;
+    } catch (error) {
+        throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
+    }
+}
+
+export const deleteItemFromInventory = async (characterId: string, itemId: number) => {
+    try {
+        const { error } = await supabase
+            .from('inventories')
+            .delete()
+            .eq('character', characterId)
+            .eq('item', itemId)
+
+        if (error) {
+            console.log(error);
+            throw new HTTPException(500, { message: 'unable to delete item from inventory' })
+        }
+
+        return true;
+    } catch (error) {
+        throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
+    }
+}
--- api/routes/crafting.ts
+++ api/routes/crafting.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { type User } from '@supabase/supabase-js';
-import { getCraftingRecipes } from "../controllers/crafting.js";
 import { combineRecipeRows } from "../utilities/functions.js";
+import { addItemToInventory } from "../controllers/inventory.js";
+import { getCraftingRecipes } from "../controllers/crafting.js";
+import { userInfo } from "os";
 
 type Variables = {
     user: { user: User };
@@ -15,6 +17,15 @@ crafting.get('/', async (c) => {
         const recipeRows = await getCraftingRecipes();
         const combinedRecieps = combineRecipeRows(recipeRows);
         return c.json(combinedRecieps);
+    } catch (error) {
+        throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
+    }
+});
+
+crafting.post('/', async (c) => {
+    try {
+        const user = c.get('user').user;
+        const item = await addItemToInventory(user.id, 4, 2);
     } catch (error) {
         throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
     }
