4b33af5c6f4854cc50f34086ea248c3362fa7be8
Grant Sherman <tnarg122@gmail.com>
2025-04-21 04:08:29 +0000

    item query string

--- api/controllers/queryStrings.ts
+++ api/controllers/queryStrings.ts
@@ -1,6 +1,17 @@
+export const getItem = `
+    id,
+    name,
+    category,
+    value,
+    description,
+    image:lk_item_images(*)
+`;
+
 export const getMonsterLoot = `
     monster_id,
-    item:items!monster_loot_item_id_fkey(*),
+    item:items!monster_loot_item_id_fkey(
+        ${getItem}
+    ),
     quantity,
     drop_probability
 `;
