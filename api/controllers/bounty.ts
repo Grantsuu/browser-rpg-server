dd061c848f5065692b23abc72e68738970fee685
Grant Sherman <tnarg122@gmail.com>
2025-05-14 18:19:38 +0000

    update bounty endpoint

--- api/controllers/bounty.ts
+++ api/controllers/bounty.ts
@@ -19,3 +19,16 @@ export const getCharacterBounties = async () => {
 
     return data;
 }
+
+export const updateBounty = async (bountyId: number, updateJson: object) => {
+    const { data, error } = await supabase
+        .from('character_bounties')
+        .update(updateJson)
+        .eq('id', bountyId)
+    if (error) {
+        console.log(error);
+        throw new HTTPException(500, { message: 'unable to update bounty' })
+    }
+
+    return data;
+}
--- api/routes/bounty.ts
+++ api/routes/bounty.ts
@@ -1,12 +1,23 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
-import { getCharacterBounties } from "../controllers/bounty.js";
+import { getCharacterBounties, updateBounty } from "../controllers/bounty.js";
 
 const bounty = new Hono();
 
 bounty.get('/', async (c) => {
     try {
         const bounty = await getCharacterBounties();
+        return c.json(bounty);
+    } catch (error) {
+        throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
+    }
+});
+
+bounty.patch('/:id', async (c) => {
+    const bountyId = c.req.param('id');
+    const updateJson = await c.req.json();
+    try {
+        const bounty = await updateBounty(parseInt(bountyId), updateJson);
         return c.json(bounty);
     } catch (error) {
         throw new HTTPException((error as HTTPException).status, { message: (error as HTTPException).message });
